bcbd5bb150f96d01f5de5e7da33197a21dc23a89
shang1128 <centrii52@gmail.com>
2021-03-25 21:14:05 +0800

    java code

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+
+//import java.util.*;
+public class Main {
+
+    public static void main (String[] args){
+
+        byte byteVariable=122;
+        int wholeNumber=122;
+        long largeDecimalNumbers=-12;
+        float numberWithDecimalplaces=8.3f;
+        double number=100;
+        boolean isTrueorFalse=true;
+        char letter='a';
+        String text="Hello World";
+
+
+        System.out.println("Primitive types");
+        System.out.println("byte "+byteVariable);
+        System.out.println("int "+wholeNumber);
+        System.out.println("long "+largeDecimalNumbers);
+        System.out.println("float "+numberWithDecimalplaces);
+        System.out.println("double "+number);
+        System.out.println("boolean "+ isTrueorFalse);
+        System.out.println("char "+letter);
+        System.out.println("Non-primitive types");
+        System.out.println(text);
+
+
+
+        double doubleNumber;
+        int i=23;
+        doubleNumber=i;
+
+        System.out.println(doubleNumber);
+    }
+}
